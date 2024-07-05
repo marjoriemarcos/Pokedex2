8253e8f991c8b6e42905db84d77940e5212560ac
Marjorie MARCOS <marjoriemarcos@mbp-de-marjorie.home>
2024-07-05 11:06:53 +0200

    finish exercice 3

--- src/components/PokemonCard.jsx
+++ src/components/PokemonCard.jsx
@@ -1,9 +1,33 @@
-function PokemonCard() {
-    return <figure>
+import styles from './PokemonCard.module.css';
 
-        <img src='https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png'></img>
-        <figcaption>Bulbasaur</figcaption>
-    </figure>;
+function PokemonCard() {
+    let pokemon = pokemonList[0];
+    return <div className={styles.container}>
+            <figure> 
+                {pokemon.imgSrc === undefined 
+                    ? 
+                    <p>???</p> 
+                    :                 
+                    <img 
+                        src={pokemon.imgSrc}
+                        alt={pokemon.name}>
+                    </img>
+                }
+
+                <figcaption className={styles.title} >{pokemon.name}</figcaption>
+            </figure>
+            </div>
   }
+
+  const pokemonList = [
+    {
+      name: "bulbasaur",
+      imgSrc:
+        "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
+    },
+    {
+      name: "mew",
+    },
+  ];
   
   export default PokemonCard;
